76928458c53f49a89aba8e05a9acc1baa4a61709
Puttapaka saikrishna <86375697+PuttapakaSaikrishna@users.noreply.github.com>
2022-06-26 15:27:44 +0530

    Update Arithmetic_calculator.java

--- Arithmetic_calculator.java
+++ Arithmetic_calculator.java
@@ -11,36 +11,44 @@ public class Arithmetic_calculator {
 		System.out.println("Enter the values ");
 		n1=sc.nextInt();
 		n2=sc.nextInt();
-		System.out.println("Select operations 1='Sub' 2='add' 3='mul' 4='div' 5='modulus'");
+		System.out.println("Select operations 1='add' 2='sub' 3='mul' 4='div' 5='modulus'");
 		ch= sc.nextInt();
+
 		
-		if(ch ==1)
-		{
-			cal =n1+n2;
-			System.out.println("Addition :"+ ch);
-			
-		}
-		else if(ch==2){
-			cal =n1-n2;
-			System.out.println("Substraction :"+ ch);
-			
-		}
-		else if(ch==3){
-			cal =n1*n2;
-			System.out.println("Multiplication :"+ ch);
-			
-		}
-		else if(ch==4){
-			cal =n1/n2;
-			System.out.println("Division :"+ ch);
-			
-		}
-		else if(ch==5){
-			cal =n1%n2;
-			System.out.println("Modulus :"+ ch);
-			
-		}
-		
-	}
+		switch (ch) {
+
+	      // performs addition between numbers
+	      case  1:
+	        cal = n1 + n2;
+	        System.out.println(n1 + " + " + n2 + " = " + cal);
+	        break;
+
+	      // performs subtraction between numbers
+	      case 2:
+	    	  cal = n1 - n2;;
+	    	  System.out.println(n1 + " - " + n2 + " = " + cal);
+	        break;
+
+	      // performs multiplication between numbers
+	      case '3':
+	    	  cal = n1 * n2;;
+	    	  System.out.println(n1 + " * " + n2 + " = " + cal);
+	        break;
+
+	      // performs division between numbers
+	      case 4:
+	    	  cal = n1 / n2;;
+	        System.out.println(n1 + " / " + n2 + " = " + cal);
+	        break;
+	        
+	      case 5:
+	    	  cal = n1 % n2;;
+	        System.out.println(n1 + " % " + n2 + " = " + cal);
+	        break;
+
+	      default:
+	    	  System.out.println("Invalid operator!");
+	          break;
+	      }
 
 }
