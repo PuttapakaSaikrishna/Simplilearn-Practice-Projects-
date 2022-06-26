6bc3d5b00440a55b6de76fd910a5c091bc9c7c4c
Puttapaka saikrishna <86375697+PuttapakaSaikrishna@users.noreply.github.com>
2022-06-26 15:36:04 +0530

    Update filles.java

--- filles.java
+++ filles.java
@@ -1,38 +1,38 @@
-package assignments;
-
+package lesson1;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class filles {
+public class Files {
 	public static void main(String[] args) throws IOException {
 		
-//*******Write the file*************
-		
-		FileWriter fw = new FileWriter("F:\\time waste\\saikrishna.txt");
-		fw.write("simplilearn \n");
-		
-//*******Append the file*************
-		fw.append("Assignment");
-		
-		fw.close();
-		
-		System.out.println("Data entered in the file successfully \n");
-		
-		
-//*******read the file*************
-		
-		FileReader fr = new FileReader("F:\\time waste\\saikrishna.txt");
-		System.out.println("*******reading the file*************");
-	        int i;
-	        while ((i = fr.read()) != -1)
-	            System.out.print((char)i);
-	        
-	        
-
-	        
+		//*******Write the file*************
+				
+				FileWriter fw = new FileWriter("F:\\time waste\\saikrishna.txt");
+				fw.write("simplilearn \n");
+				
+				System.out.println("*******Writing the file************* \n");
+				System.out.println(" simplilearn \n");
+				
+		//*******Append the file*************
+				fw.append("Assignment");
+				
+				fw.close();
+				System.out.println("*******Appending the file*************\n");
+				System.out.println("Assignment \n");
+				
+				
+				System.out.println("** Data entered into the file successfully ** \n");
+				
+				
+		//*******read the file*************
+				
+				FileReader fr = new FileReader("F:\\time waste\\saikrishna.txt");
+				System.out.println("*******reading the file************* \n");
+			        int i;
+			        while ((i = fr.read()) != -1)
+			            System.out.print((char)i);
 	       
-	    }
-	}
+			    }
 
-
+}
